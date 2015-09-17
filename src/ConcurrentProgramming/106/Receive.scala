6b1dad9ab770c26f87c38f7d1f8a6f24d0300dbd
suqun <215144509@qq.com>
2015-09-17 22:49:32 +0800

    receive

---
+++ src/ConcurrentProgramming/106/Receive.scala
@@ -0,0 +1,36 @@
+/**
+ * Created by larry on 9/17/15.
+ * receive 和 receiveWithin
+ * receive 接收一个函数值/闭包，返回一个处理消息的应答
+ *
+ */
+
+import scala.actors.Actor._
+
+val caller = self
+
+val accumulator = actor {
+  var sum = 0
+  var continue = true
+  while (continue) {
+    sum += receive {
+      case number: Int => number
+      case "quit" =>
+        continue = false
+        0
+    }
+  }
+  caller ! sum
+}
+
+accumulator ! 1
+accumulator ! 7
+accumulator ! 8
+accumulator ! "quit"
+
+receive { case result => println("Total is " + result)}
+
+/*
+Total is 16
+ */
+
